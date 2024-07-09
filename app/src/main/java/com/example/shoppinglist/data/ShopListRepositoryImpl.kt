bb565705758b40f2853c46265214c503804ef3e4
Vanillaa00 <i.zavyalov2006@gmail.com>
2024-07-09 23:02:13 +0400

    creating data

---
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,37 @@
+package com.example.shoppinglist.data
+
+import com.example.shoppinglist.domain.ShopItem
+import com.example.shoppinglist.domain.ShopListRepository
+
+class ShopListRepositoryImpl: ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+    private var AutoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = AutoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(id: Int): ShopItem {
+        return shopList.find {
+            it.id == id
+        } ?: throw RuntimeException("Element with id $id not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+}
